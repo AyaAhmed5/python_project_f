6f67d632b63d0d8a078fa8efc60d031d726da18a
Aya ahmed <93159662+AyaAhmed5@users.noreply.github.com>
2022-12-17 23:57:15 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,62 @@
+'''
+#Q2
+num1=input('Enter num1')
+num2=input('Enter num3')
+num1=float(num1)
+num2=float(num2)
+print( 'result=', num1/num2 )'''
+
+
+"""
+#q1
+num1=float(47.28)
+print('tips=' ,.15*num1)
+tibs= num1*.15
+print('total=', tibs+num1)
+total=num1+tibs
+print('Each person needs to pay', total/2)"""
+
+"""
+#Q3
+word1 = "How"
+word2 = "do"
+word3 = "you"
+word4 = "like"
+word5 = "Python"
+word6 = "so"
+word7 = "far?"
+print(word1,word2,word3,word4,word5,word6,word7)'''
+'''
+#Q4
+name = 'aya ahmed {}'
+print (name.format("mohamed"))
+name = 'aya ahmed '
+print(name.replace('a','A'))
+'''
+
+'''
+#q5
+
+name = 'fantastic'
+print( 'the length of a string=',len(name))
+'''
+
+'''
+#q6
+name=input('enter your name')
+age = float(input('enter your age'))
+address=input('enter your address')
+college=input('enter your college')
+telephone=input('your telephone')
+print('name:',name)
+print('age:',age)
+print('address:',address)
+print('college:',college)
+print('telephone:',telephone)
+'''
+
+
+name='mobile'
+print(name.replace('o','').replace('i','').replace('a','').replace('e','').replace('u',''))
+"""
+
